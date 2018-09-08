54ef237dedbb61c653906d72ee30285f34843dd0
goutam palkar <goutampalkar6336@gmail.com>
2018-09-08 18:24:23 +0530

    new changes

--- user_Reg_Var.php
+++ user_Reg_Var.php
@@ -1,4 +1,34 @@
 <?php
+$Name=$_POST['RegName'];
+$Email=$_POST['RegMail'];
+$Mob=$_POST['RegNum'];
+$Password=$_POST['RegPass'];
+$ConfPassword=$_POST['RegConfPass'];
+
+$Course=$_POST['RegCourse'];
+include('db_Config.php');
+
+if($Name=="" || $Email=="" || $Mob=="" || $Password=="" || $ConfPassword=="" || $Course=="")
+{
+	echo "Please enter valid field";
+
+}
+else
+{
+
+// $query=mysql_query("insert into userregistration values('NULL','$Name','$Mob','$Course','$Email','$Password','$ConfPassword')");
+// 		if( $query )
+// 		{
+// 			echo " inserted";
+// 		}
+// 		else
+// 		{
+// 			echo "something is wrong";
+// 		}
+	echo "inseted";
+}
+
+
 
 
 ?>
