3cfb7d89e536493aae86428472c0221736122ad6
W2T001 <w2t@gmail.com>
2018-09-10 17:07:42 +0530

    data base setting chagens

--- db_Config.php
+++ db_Config.php
@@ -1,10 +1,14 @@
 <?php
-$cn=mysql_connect("192.168.31.40","dms","dms");
- mysql_select_db("user");
-
+// echo "php";
+$cn=mysqli_connect("192.168.31.40","dms","dms");
+ mysqli_select_db("user");
+// echo "data";
  if(!$cn)
 		{
 		 die("connection failed".mysql_connect_error());
+		}
+		else{
+			// var_dump("data");
 		}
 		
 
--- user_Reg_Var.php
+++ user_Reg_Var.php
@@ -9,34 +9,66 @@ $Email=$_POST['RegMail'];
 $Mob=$_POST['RegNum'];
 $Password=$_POST['RegPass'];
 $ConfPassword=$_POST['RegConfPass'];
-
+echo $Name,$Email,$Password,$Mob;
 $Course=$_POST['RegCouse'];
+
 include('db_Config.php');
+
+echo "step1";
+
 
 if($Name=="" || $Email=="" || $Mob=="" || $Password=="" || $ConfPassword=="" || $Course=="")
 {
 	echo "Please enter valid field";
 
 }
-else
-{
+// else
+// {
 
- $query=mysql_query("insert into userregistration values('NULL','$Name','$Mob','$Course','$Email','$Password','$ConfPassword')");
- 		if( $query )
-		{
-			header('location:user_login.php');
-		}
- 		else
-		{
- 			die("something is wrong:".mysql_error()); 
- 		}
+//  $query=mysqli_query("insert into userregistration values('NULL','$Name','$Mob','$Course','$Email','$Password','$ConfPassword')");
+//  		if( $query )
+// 		{
+// 			header('location:user_login.php');
+// 		}
+//  		else
+// 		{
+//  			die("something is wrong:".mysql_error()); 
+//  		}
 	
-}
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
+// }
 
->>>>>>> 302c3ee8a20625e4f51131277ccbf890528a8fa0
->>>>>>> b9f29d81bba9032d0e4a622ca2fa3dafb3d3f816
+
+
+$servername = "localhost";
+$username = "dms";
+$password = "dms";
+$dbname = "user";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+// $sql = "INSERT INTO MyGuests (firstname, lastname, email) VALUES ('John', 'Doe', 'john@example.com')";
+
+$sql = "insert into userregistration (RegName, RegMob, RegCourse, RegEmail, RegPswd, RegConfPaswd) values('$Name','$Mob','$Course','$Email','$Password','$ConfPassword')";
+
+var_dump($sql);
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+
+
+
+
+
 ?>
