bc33660d9dc80bcaa6d5e2253f6ec5144955e55b
goutam palkar <goutampalkar6336@gmail.com>
2018-09-10 14:15:59 +0530

    ready to  login page

--- user_login.php
+++ user_login.php
@@ -1,49 +1,5 @@
-<?php
 
 
-/*here we started the session*/
-
-session_start();
-
-
-include('dbcon.php');
-
-
-if(isset($_GET['email']))
-
-{
-    $email=$_GET['email'];
-    $password=$_GET['pass'];
-
-    $sql = "SELECT * FROM user_register WHERE email='".$email."' AND
-    password='".$password."'";
-
-    $res =mysql_query($sql) or die("error to find data");
-
-    $num_rows=mysql_num_rows($res);
-
-
-    if($num_rows==1)
-    
-    {
-        /*user is session name*/
-
-        $_SESSION['user'] = $email;
-        header('location:userprofile.php');
-            exit;
-
-    }
-    else
-    {
-
-
-    ?> <script> alert("invalid user name or password"); </script>
-
-<?php
-    }
-
-    }
-?>
 
 <html>
     <head>
@@ -65,16 +21,16 @@ if(isset($_GET['email']))
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/bootstrap.min.css">
  
-    </head><br>
+    </head><body><br>
     <div class="b">
-      <a href="index.php">   <input type="button"
+      <a href="user_registration.php">   <input type="button"
 class="btn btn-primary" value="Sign up">  </a></div>
 
-    <body>
+    
  <div class="container">
     <h1 align="center" > Welcome to User Login  </h1><br><br>
             
-    <form action="login.php" method="get">
+    <form action="loginVarify.php" method="POST">
         <div class="form-group">
        
         <label>Email: </label>
