b019b30f97550f2ad6fb7bb9827d66188b3e6e9b
nitin kadam <kadamnitin314@gmail>
2018-09-10 17:06:55 +0530

    changes

--- db_Config.php
+++ db_Config.php
@@ -1,11 +1,14 @@
+
+
+
 <?php
-$cn=mysql_connect("localhost","root","hitler");
+
+ $cn=mysql_connect("localhost","root","hitler");
  mysql_select_db("user");
 
  if(!$cn)
-		{
+	
+ {
 		 die("connection failed".mysql_connect_error());
 		}
-		
-
 ?>
