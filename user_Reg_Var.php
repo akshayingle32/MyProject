caad66621bc4e547307ecfedc866f6ede24e82bf
goutam palkar <goutampalkar6336@gmail.com>
2018-09-11 11:54:58 +0530

    add varify resitrer page

--- user_Reg_Var.php
+++ user_Reg_Var.php
@@ -1,40 +1,59 @@
 
 <?php
-$Name=$_POST['RegName'];
+
+if($_SERVER['REQUEST_METHOD']=="POST")
+{
+	$Name=$_POST['RegName'];
 $Email=$_POST['RegMail'];
 $Mob=$_POST['RegNum'];
 $Password=$_POST['RegPass'];
 $ConfPassword=$_POST['RegConfPass'];
 
 $Course=$_POST['RegCouse'];
-include('db_Config.php');
-
-if($Name=="" || $Email=="" || $Mob=="" || $Password=="" || $ConfPassword=="" || $Course=="")
-{
-	echo "Please enter valid field";
-
+ echo "Server method is POST";
 }
-else
-{
+elseif ($_SERVER['REQUEST_METHOD']=="GET")
+ {
+	$Name=$_POST['RegName'];
+$Email=$_POST['RegMail'];
+$Mob=$_POST['RegNum'];
+$Password=$_POST['RegPass'];
+$ConfPassword=$_POST['RegConfPass'];
 
- $query=mysql_query("insert into userregistration values('NULL','$Name','$Mob','$Course','$Email','$Password','$ConfPassword')");
- 		if( $query )
-		{
-			header('location:user_login.php');
-		}
- 		else
-		{
- 			die("something is wrong:".mysql_error()); 
- 		}
+$Course=$_POST['RegCouse'];
+ echo "Server method is GET";
+}
 
- 		
+
+// $Name=$_POST['RegName'];
+// $Email=$_POST['RegMail'];
+// $Mob=$_POST['RegNum'];
+// $Password=$_POST['RegPass'];
+// $ConfPassword=$_POST['RegConfPass'];
+
+// $Course=$_POST['RegCouse'];
+// include('db_Config.php');
+
+// if($Name=="" || $Email=="" || $Mob=="" || $Password=="" || $ConfPassword=="" || $Course=="")
+// {
+// 	echo "Please enter valid field";
+
+// }
+// else
+// {
+
+//  $query=mysqli_query("insert into userregistration values('NULL','$Name','$Mob','$Course','$Email','$Password','$ConfPassword')");
+//  		if( $query )
+// 		{
+// 			header('location:user_login.php');
+// 		}
+//  		else
+// 		{
+//  			die("something is wrong:".mysqli_error()); 
+//  		}
+
+
 	
-}
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
+// }
 
->>>>>>> 302c3ee8a20625e4f51131277ccbf890528a8fa0
->>>>>>> b9f29d81bba9032d0e4a622ca2fa3dafb3d3f816
 ?>
